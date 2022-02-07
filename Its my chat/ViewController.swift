b660da342e2f497f2502422bd3d253a5c38b2aea
l40perl <l40perl@gmail.com>
2022-02-07 23:40:24 +0300

    creat project

---
+++ Its my chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Its my chat
+//
+//  Created by MacBook on 06.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
